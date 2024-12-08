047e15a4c07e217b37a454357fd5a0ae95cec6a5
wuminglangren <wuminglangren@163.com;wuminglangren03@gmail.com;wuminglangren03@outlook.com>
2024-12-08 15:16:30 -0500

    Solve a werid bug.

--- display.py
+++ display.py
@@ -1176,7 +1176,7 @@ else:
             information['query'] = f" SELECT customer_city, COUNT(order_id) AS total_orders FROM ({base_query['body']}) AS subquery GROUP BY customer_city ORDER BY total_orders DESC;"
             information['data'] = base_query['value']
             value = st.session_state.connector.query(information['query'], information['data'])
-            information['result'] = alt.Chart(value).mark_bar().encode(x=information["index"]["x"], y = information['index']['y'])
+            information['result'] = alt.Chart(value).mark_bar().encode(x=alt.X(information["index"]["x"], sort=None), y = information['index']['y'])
             querys.append(information)
             pass
             sleep(0.1)
